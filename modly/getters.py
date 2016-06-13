8423b431756576f74410e74ee2997f7d5cb26f95
Brad Smith <brad.alan.smith@gmail.com>
2016-06-13 14:31:30 -0700

    github module!

--- modly/getters.py
+++ modly/getters.py
@@ -1,12 +1,27 @@
 
-import os
+import os, base64, imp, sys, string, random
 
 from github import Github
 
 here = os.path.dirname(os.path.abspath(__file__))
-
 g = Github(open(os.path.join(here,'..','.github_token'), 'r').read())
 
-repo = g.get_repo('cacahootie/modly')
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
-print repo.get_contents('test/tmod.py').content
+def get_github_string(user, repo, fname):
+    return base64.b64decode(
+        g.get_repo('{}/{}'.format(user, repo))\
+            .get_contents('test/tmod.py').content
+    )
+
+def get_github_module(user, repo, fname, module_name=None):
+    return get_module(
+        get_github_string(user, repo, fname),
+        module_name
+    )
+
+def get_module(module_string, module_name=None):
+    new_module = imp.new_module(module_name if module_name else id_generator())
+    exec module_string in new_module.__dict__
+    return new_module
--- test/test.py
+++ test/test.py
@@ -1,10 +1,23 @@
 import unittest
 
-import modly
+import modly.getters as getters
 
 class TestImport(unittest.TestCase):
 
     def test_import_string(self):
         modstr = "a = 5"
-        newmod = modly.get_module(modstr)
+        newmod = getters.get_module(modstr)
         self.assertEqual(newmod.a, 5)
+
+    def test_github_string(self):
+        teststr = getters.get_github_string(
+            'cacahootie', 'modly', 'test/tmod.py'
+        )
+        self.assertTrue("a = 11" in teststr)
+
+    def test_github_module(self):
+        testmod = getters.get_github_module(
+            'cacahootie', 'modly', 'test/tmod.py', 'tmod'
+        )
+        self.assertEqual(testmod.a, 11)
+        self.assertEqual(testmod.b, 12)
