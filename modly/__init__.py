c60ae2881fd4d20123f1a8fe6bb6ad0aafda89e7
Brad Smith <brad.alan.smith@gmail.com>
2016-06-13 12:45:38 -0700

    it does something now

--- modly/__init__.py
+++ modly/__init__.py
@@ -1,1 +1,10 @@
 
+import imp, sys, string, random
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def get_module(module_string, module_name = None):
+    new_module = imp.new_module(module_name if module_name else id_generator())
+    exec module_string in new_module.__dict__
+    return new_module
--- test/test.py
+++ test/test.py
@@ -1,1 +1,10 @@
 import unittest
+
+import modly
+
+class TestImport(unittest.TestCase):
+
+    def test_import_string(self):
+        modstr = "a = 5"
+        newmod = modly.get_module(modstr)
+        self.assertEqual(newmod.a, 5)
